ae985696bb29df7e1e03c7f483ce8ab80e234882
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-15 10:41:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,10 +30,10 @@ attr_reader :id
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?) 
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
       SQL
-      DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
 end
